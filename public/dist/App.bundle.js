fcaac7e61e2f1a5c87d5a23691b4be097988090a
seunzone <darealseun@gmail.com>
2017-10-30 20:43:06 +0100

    starter files

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -63,55 +63,14 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-// based on https://gist.github.com/paulirish/12fb951a8b893a454b32
-
-var $ = document.querySelector.bind(document);
-var $$ = document.querySelectorAll.bind(document);
-
-Node.prototype.on = window.on = function (name, fn) {
-  this.addEventListener(name, fn);
-};
-
-NodeList.prototype.__proto__ = Array.prototype; // eslint-disable-line
-
-NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
-  this.forEach(function (elem) {
-    elem.on(name, fn);
-  });
-};
-
-exports.$ = $;
-exports.$$ = $$;
-
-/***/ }),
-/* 1 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: Error\n    at E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\NormalModule.js:141:35\n    at E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:170:18\n    at loadLoader (E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\loadLoader.js:27:11)\n    at iteratePitchingLoaders (E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:169:2)\n    at iteratePitchingLoaders (E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:165:10)\n    at E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:173:18\n    at loadLoader (E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\loadLoader.js:36:3)\n    at iteratePitchingLoaders (E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:169:2)\n    at iteratePitchingLoaders (E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:165:10)\n    at E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:173:18\n    at loadLoader (E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\loadLoader.js:36:3)\n    at iteratePitchingLoaders (E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:169:2)\n    at runLoaders (E:\\Node Sites\\noderestaurantfinder\\node_modules\\loader-runner\\lib\\LoaderRunner.js:362:2)\n    at NormalModule.doBuild (E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\NormalModule.js:129:2)\n    at NormalModule.build (E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\NormalModule.js:180:15)\n    at Compilation.buildModule (E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\Compilation.js:142:10)\n    at moduleFactory.create (E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\Compilation.js:424:9)\n    at E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\NormalModuleFactory.js:242:4\n    at E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\NormalModuleFactory.js:93:13\n    at E:\\Node Sites\\noderestaurantfinder\\node_modules\\tapable\\lib\\Tapable.js:204:11\n    at NormalModuleFactory.params.normalModuleFactory.plugin (E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\CompatibilityPlugin.js:52:5)\n    at NormalModuleFactory.applyPluginsAsyncWaterfall (E:\\Node Sites\\noderestaurantfinder\\node_modules\\tapable\\lib\\Tapable.js:208:13)\n    at onDoneResolving (E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\NormalModuleFactory.js:68:11)\n    at onDoneResolving (E:\\Node Sites\\noderestaurantfinder\\node_modules\\webpack\\lib\\NormalModuleFactory.js:189:6)\n    at _combinedTickCallback (internal/process/next_tick.js:95:7)\n    at process._tickCallback (internal/process/next_tick.js:161:9)");
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-__webpack_require__(1);
-
-var _bling = __webpack_require__(0);
+throw new Error("Module build failed: ReferenceError: Unknown plugin \"transform-object-rest-spread\" specified in \"E:\\\\Node Sites\\\\NodeRestaurantFinder\\\\.babelrc\" at 1, attempted to resolve relative to \"E:\\\\Node Sites\\\\NodeRestaurantFinder\"\n    at E:\\Node Sites\\NodeRestaurantFinder\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:180:17\n    at Array.map (native)\n    at Function.normalisePlugins (E:\\Node Sites\\NodeRestaurantFinder\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:158:20)\n    at OptionManager.mergeOptions (E:\\Node Sites\\NodeRestaurantFinder\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:234:36)\n    at OptionManager.init (E:\\Node Sites\\NodeRestaurantFinder\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:368:12)\n    at File.initOptions (E:\\Node Sites\\NodeRestaurantFinder\\node_modules\\babel-core\\lib\\transformation\\file\\index.js:212:65)\n    at new File (E:\\Node Sites\\NodeRestaurantFinder\\node_modules\\babel-core\\lib\\transformation\\file\\index.js:135:24)\n    at Pipeline.transform (E:\\Node Sites\\NodeRestaurantFinder\\node_modules\\babel-core\\lib\\transformation\\pipeline.js:46:16)\n    at transpile (E:\\Node Sites\\NodeRestaurantFinder\\node_modules\\babel-loader\\lib\\index.js:46:20)\n    at Object.module.exports (E:\\Node Sites\\NodeRestaurantFinder\\node_modules\\babel-loader\\lib\\index.js:155:20)");
 
 /***/ })
 /******/ ]);
